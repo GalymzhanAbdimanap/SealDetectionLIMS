5b04b618a5d1325158d8a232ab0a347518b25df8
abdi <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2020-08-04 15:00:12 +0600

    Update parsers.py

--- parsers.py
+++ parsers.py
@@ -2,12 +2,14 @@
 import werkzeug
 from flask_restplus import reqparse
 
+# Requirement for input file and for show on swagger
 file_upload = reqparse.RequestParser()
 file_upload.add_argument('File',  
                          type=werkzeug.datastructures.FileStorage, 
                          location='files', 
                          required=True, 
                          help='PDF file')
+# Requirement for input file and for show on swagger
 id_of_file = reqparse.RequestParser()
 id_of_file.add_argument('ID',
                          type=int,
